f96fd777f8cb3b10b2207d771c53ef7a98c3cd4d
JohnVel27 <124770839+JohnVel27@users.noreply.github.com>
2024-10-03 15:21:09 +0800

    Create dbConfig.php

---
+++ dbConfig.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "cashflows";
+$dsn = "mysql:host={$host};dbname={$dbname}";
+
+$conn = new PDO($dsn,$user,$password);
+$conn->exec("SET time_zone = '+08:00';");
+
+if(mysqli_connect_error()){
+    echo 'error';
+}
+?>
