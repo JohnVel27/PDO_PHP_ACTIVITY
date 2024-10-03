c466a10739ce7a5f30cff31e32c2ddb708f64602
JohnVel27 <124770839+JohnVel27@users.noreply.github.com>
2024-10-03 15:21:40 +0800

    Update dbConfig.php

--- dbConfig.php
+++ dbConfig.php
@@ -8,6 +8,8 @@ $dsn = "mysql:host={$host};dbname={$dbname}";
 $conn = new PDO($dsn,$user,$password);
 $conn->exec("SET time_zone = '+08:00';");
 
+
+// Check Connection
 if(mysqli_connect_error()){
     echo 'error';
 }
